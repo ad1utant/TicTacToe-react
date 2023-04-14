5ebea7804f238001b87e3aa5abca346e2b126167
adiutant <mikolaj@marasek.eu>
2023-04-14 10:34:49 +0200

    now u can see buttons

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import Button from "./button.jsx";
 
 function Board() {
-    const =[buttons,setButtonsa]
+    const list = ['a1','a2','a3','b1','b2','b3','c1','c2','c3'];
     return (
+        <div>
+            {list.map(el =>(
+            <button key ={el} className={'button'}>o/x</button>
+            ))}
+        </div>
     );
 }
 
